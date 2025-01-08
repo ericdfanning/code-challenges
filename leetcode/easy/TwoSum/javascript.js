b9fefa4c323db41a4d1407f2b4b717df9fc297fe
Eric Fanning <ericdfanning@gmail.com>
2025-01-08 15:29:56 -0600

    two sum clean up and notes

--- leetcode/easy/TwoSum/javascript.js
+++ leetcode/easy/TwoSum/javascript.js
@@ -4,16 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-  let indices = []
   for (let i = 0; i < nums.length; i++) {
       let possibleIndex = nums.indexOf(target - nums[i])
       if (possibleIndex !== -1 && possibleIndex !== i) {
-          indices.push(i, possibleIndex)
-          break
+          return [i, possibleIndex]
       }
   }
-  return indices;
+  return []
 };
 
 // Looked at answers first? No
-// doesn't rank well in terms of speed/runtime. 
+// doesn't rank well in terms of speed/runtime. I would need to prevent duplicate loops or checks.
+// indexOf could be slower than a nested loop if it's a large data set. 
+// use i to help set j in order to avoid checking numbers already evaluated in the inverse
