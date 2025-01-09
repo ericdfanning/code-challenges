c077d51520e02c8400919c0538a50048576dc8d5
Eric Fanning <ericdfanning@gmail.com>
2025-01-09 16:50:53 -0600

    longest substring without repeating characters

---
+++ leetcode/medium/LongestSubstringWithoutRepeatingCharacters/javascript.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+  let map = new Map()
+  const split = s.split('')
+  let longestLength = 0;
+  let subStringLength = 0;
+  if (split.length === 1) return 1
+  for (let i = 0; i < split.length; i++) {
+      if (map.has(split[i])) {
+          if (subStringLength > longestLength) {
+              longestLength = subStringLength
+          }
+          subStringLength = 0
+          i = map.get(split[i])
+          map.clear()
+          continue
+      }
+      map.set(split[i], i)
+      subStringLength += 1
+      if (i === split.length - 1 && subStringLength > longestLength) {
+          longestLength = subStringLength
+      }
+  }
+  return longestLength
+};
