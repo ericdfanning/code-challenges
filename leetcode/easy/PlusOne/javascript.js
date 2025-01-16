b0f0f7f0595c42bea32c5b94c3d91fad5ac45ae3
Eric Fanning <ericdfanning@gmail.com>
2025-01-16 17:01:29 -0600

    plus one

---
+++ leetcode/easy/PlusOne/javascript.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  // const stringResult = `${(Number(digits.join('')) + 1)}`.split('')
+  // return stringResult.map(num => Number(num))
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] !== 9) {
+          digits[i] = digits[i] + 1
+          for (let j = i + 1; j < digits.length; j++) {
+              digits[j] = 0
+          }
+          break;
+      } else if (i === 0) {
+          digits[i] = 1
+          for (let j = i + 1; j < digits.length; j++) {
+              digits[j] = 0
+          }
+          digits.push(0)
+          break;
+      }
+  }
+ 
+  return digits
+};
