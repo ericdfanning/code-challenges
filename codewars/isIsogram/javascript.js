db1ced74a45223c4bdf7af394b172c76d028b39a
Eric Fanning <ericdfanning@gmail.com>
2026-02-03 17:58:50 -0600

    alternate solution that's clever from com

--- codewars/isIsogram/javascript.js
+++ codewars/isIsogram/javascript.js
@@ -8,3 +8,10 @@ function isIsogram(str){
   }
   return true
 }
+
+// this is a clever solution by someone else in the community. 
+// By making it toUpperCase, a new set takes a strings individual letters and makes it a new node
+// which cuts out duplicates because it's a set
+function isIsogram(str){
+	return new Set(str.toUpperCase()).size == str.length;
+}
