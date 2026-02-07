c44272b5cfebf6fc8dca49a71eb67c7e48c4805e
Eric Fanning <ericdfanning@gmail.com>
2026-02-06 23:55:18 -0600

    added another way ot do it

--- codewars/digitPower/javascript.js
+++ codewars/digitPower/javascript.js
@@ -8,3 +8,13 @@ function digPow(n, p){
   const k = total/n
   return k >= 1 && Math.trunc(k) === k ? k : -1
 }
+
+function digPow(n, p){
+  let nums = String(n).split('')
+  let total = 0
+  for (let i = 0; i < nums.length; i++) {
+    total += Math.pow(nums[i], p+i)
+  }
+
+  return total%n >= 1 ? -1 : total/n
+}
