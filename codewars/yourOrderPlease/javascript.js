e3af2cc905c9363cdf03bb881449516b0a8ccab1
Eric Fanning <ericdfanning@gmail.com>
2026-02-12 17:02:44 -0600

    other way found from others

--- codewars/yourOrderPlease/javascript.js
+++ codewars/yourOrderPlease/javascript.js
@@ -7,3 +7,13 @@ function order(words){
   }
   return words.split(' ').sort(sortFunc).join(' ')
 }
+
+// other answers showed I could have made the logic simpler and I didn't need to 
+// get the number from the match result but could compare directly
+function order(words){
+  if (!words) return ''
+  const sortFunc = (a, b) => {
+    return a.match(/\d/) - b.match(/\d/)
+  }
+  return words.split(' ').sort(sortFunc).join(' ')
+}
