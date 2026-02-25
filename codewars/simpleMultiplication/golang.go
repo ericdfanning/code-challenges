6684e82efb8512a57d81ae67896431236ef0f76f
Eric Fanning <ericdfanning@gmail.com>
2026-02-25 07:13:04 -0600

    clever solution

--- codewars/simpleMultiplication/golang.go
+++ codewars/simpleMultiplication/golang.go
@@ -6,3 +6,9 @@ func SimpleMultiplication(n int) int {
   }
   return n*9
 }
+
+// someone elses below, is pretty smart. It's clean because 9 becomes the default
+// and if it's 0, then it has no effect and works for both
+func SimpleMultiplication(n int) int {
+  return n * (8 + n % 2)
+}
